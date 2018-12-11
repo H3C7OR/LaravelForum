b3e0cd6b9cf128ee6d72269c97bbc95f6ab4ebb2
Hector P <hrpo.17@gmail.com>
2018-12-11 11:22:53 -0400

    A User may response to threads - final

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -10,11 +10,11 @@
                 {{ $thread->title }} 
                 </div>
 
-                <div class="card-body">
-                
-                   {{ $thread->body }}
+                <div class="card-body">                
+                   {{ $thread->body }}                   
                 </div>
             </div>
+            <br> 
         </div>
     </div>
 
@@ -22,9 +22,28 @@
         <div class="col-md-8">
             @foreach ($thread->replies as $reply)
                 @include ('threads.reply')
+                <br>
             @endforeach
+            
         </div>
     </div>
+
+    @if(auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">                        
+                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>                    
+                    </div>
+
+                    <button type="submit" class="btn btn-default">Post</button>
+                </form>
+            </div>
+        </div>
+    @endif
+    
+    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in  this discussion.</p>
 
 </div>
 @endsection
